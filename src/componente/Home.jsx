b95467f7ad47c4d64dc12b0d33a932aaf54071ef
Juan Granja <4684593@gmail.com>
2024-10-17 15:40:27 -0500

    se  modifico el home y se agregaron carpetas

--- src/componente/Home.jsx
+++ src/componente/Home.jsx
@@ -10,8 +10,24 @@ export function Home({ user, setUser }) {
             <div className="welcome-message">
                 <h1>FICHAS TECNICAS PROVEEDORES</h1>
                 <h2>Hola, {user[0]}!</h2>
+                <div className="pdf-container">
+                    <img 
+                        src="/images/Carvajal.png" 
+                        className="proveedor-image" 
+                    />
+                    <a 
+                        href="/pdf/triggers.pdf" 
+                        download="triggers.pdf" 
+                        className="download-link"
+                        type="document/pdf"
+                    >
+                        Descargar
+                    </a>
+                </div>
             </div>
-            <button className="logout-button" onClick={handleLogout}>Cerrar Sesión</button>
+            <button className="logout-button" onClick={handleLogout}>
+                Cerrar Sesión
+            </button>
         </div>
     );
 }
